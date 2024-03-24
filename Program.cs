3d4ca2ed4e9395aafe2e4b5a406f2287c3cb3602
Felippe Santana MAC <f-elipp-3@hotmail.com>
2024-03-24 15:09:04 +0000

    Modal eliminar (ClienteItem) adicionado.

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ClienteItem}/{action=Index}");
 app.MapRazorPages();
 
 app.Run();
